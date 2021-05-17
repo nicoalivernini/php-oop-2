686b2d15715cf1516d183dd94e8320a2ceceb6b6
Nicolò Alivernini <alivernininicolo@gmail.com>
2021-05-17 19:30:24 +0200

    Esercizio completato

--- index.php
+++ index.php
@@ -1,16 +1,15 @@
 <?php
 
   require_once './utente.php';
-  // require_once './prodotto.php';
-  // require_once './felpa.php';
+  require_once './prodotto.php';
+  require_once './carta-di-credito.php';
 
 
   $nico = new Utente('Nicolo', 'Alivernini', 'Maschio', '19-09-1995', 'Carta di credito', '5333-0000-0000-0000', 'Nicolò Alivernini', '05-24', 'Mastercard', '123');
-  // $prodotto1 = new Felpa('H&M', 'L', '19.99', 'Felpa', 'Made in China', 'Vestiti', 'Disponibile');
+  $felpa= new Felpa('Felpa', 'Made in China', 'Vestiti', 'Disponibile', 'H&M', 'L', '19.99');
 
-  // $prodotto2 = new Prodotto('Vestiario', 'T-shirt', 'H&M', 'M', 'Made in China', '19.99', 'Cura della persona', 'Non disponibile');
 
-  var_dump($nico, $prodotto1);
+  var_dump($nico, $felpa);
 
 
  ?>
--- prodotto.php
+++ prodotto.php
@@ -1,10 +1,10 @@
 <?php
 
   class Prodotto {
-    public $tipologia;
-    public $fabbricazione;
-    public $categoria;
-    public $status;
+    private $tipologia;
+    private $fabbricazione;
+    private $categoria;
+    private $status;
 
     // Construct
     public function __construct($tipologia, $fabbricazione, $categoria, $status) {
@@ -65,5 +65,50 @@
     }
 
   } // Chiusura Prodotto
+
+  class Felpa extends Prodotto {
+    private $marca;
+    private $misura;
+    private $prezzo;
+
+    public function __construct($tipologia, $fabbricazione, $categoria, $status, $marca, $misura, $prezzo) {
+      $this->setTipologia($tipologia);
+      $this->setFabbricazione($fabbricazione);
+      $this->setCategoria($categoria);
+      $this->setStatus($status);
+      $this->marca = $marca;
+      $this->misura = $misura;
+      $this->prezzo = $prezzo;
+    }
+
+    // Marca
+    public function setMarca($marca) {
+      $this->marca = $marca;
+    }
+
+
+    public function getMarca() {
+      return $marca->marca;
+    }
+
+    // Misura
+    public function setMisura($misura) {
+      $this->misura = $misura;
+    }
+
+    public function getMisura() {
+      return $misura->misura;
+    }
+
+    // Prezzo
+    public function setPrezzo($prezzo) {
+      $this->prezzo = $prezzo;
+    }
+
+    public function getPrezzo() {
+      return $prezzo->prezzo;
+    }
+
+  } // Chiusura Felpa
 
  ?>
